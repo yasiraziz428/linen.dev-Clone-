9f32240862b0414f45ac42a0dffa8a98c6611536
yasiraziz428 <89304808+yasiraziz428@users.noreply.github.com>
2022-12-19 00:51:03 +0500

    Add files via upload

---
+++ JS/client.js
@@ -0,0 +1,30 @@
+const socket = io("http://localhost:8000");
+
+const form = document.getElementById("send-form");
+const messageInput = document.getElementById("message-input");
+const messageContainer = document.querySelector(".container");
+
+function append(name, message) {
+  const messageHTML = `<div class="flex hover:bg-gray-100 w-full py-3 pl-3"><div><img src="/images/user-img.png" class="w-10" /></div><div class="ml-3"><span class="font-semibold">${name}</span><p>${message}</p></div></div>`;
+  const messageElement = document.createElement("div");
+  messageElement.innerHTML = messageHTML;
+  messageElement.classList.add("message");
+  messageContainer.append(messageElement);
+  //   messageContainer.scrollTop = messageContainer.scrollHeight;
+}
+
+const userName = prompt("Enter your name to join chat");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  append("me", message);
+  socket.emit("send", message);
+  messageInput.value = "";
+});
+
+socket.emit("new-user-joined", userName);
+
+socket.on("receive", (data) => {
+  append(data.name, data.message);
+});
---
+++ nodeServer/index.js
@@ -0,0 +1,23 @@
+// node server which will handle socket io connections
+
+const io = require("socket.io")(8000);
+const users = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (name) => {
+    console.log("New user", name);
+    users[socket.id] = name;
+    console.log(users);
+  });
+
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      name: users[socket.id],
+      message: message,
+    });
+  });
+
+  socket.on("disconnect", (message) => {
+    delete users[socket.id];
+  });
+});
